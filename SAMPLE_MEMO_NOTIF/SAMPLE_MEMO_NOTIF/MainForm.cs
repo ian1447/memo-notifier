52b64ccd4f68ae1e4b2e72488c1c0111b68a7502
Kitt Cymonne Japson <japson.kittcymonne@gmail.com>
2022-09-05 13:58:30 +0800

    sd

---
+++ SAMPLE_MEMO_NOTIF/SAMPLE_MEMO_NOTIF/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace SAMPLE_MEMO_NOTIF
+{
+    public partial class MainForm : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
