82d37b570045e0741910edc2d9e6c72f73a115f6
Ric Ian Jamora <yan2_1447@yahoo.com>
2022-09-05 14:19:05 +0800

    change

--- SAMPLE_MEMO_NOTIF/SAMPLE_MEMO_NOTIF/Form1.Designer.cs
+++ SAMPLE_MEMO_NOTIF/SAMPLE_MEMO_NOTIF/Form1.Designer.cs
@@ -28,33 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Title = new System.Windows.Forms.Label();
+            this.LogIn_Page = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Trebuchet MS", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.Maroon;
+            this.Title.Location = new System.Drawing.Point(12, 37);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(297, 37);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "MEMO NOTIFICATION";
+            // 
+            // LogIn_Page
+            // 
+            this.LogIn_Page.AutoSize = true;
+            this.LogIn_Page.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogIn_Page.Location = new System.Drawing.Point(116, 88);
+            this.LogIn_Page.Name = "LogIn_Page";
+            this.LogIn_Page.Size = new System.Drawing.Size(64, 23);
+            this.LogIn_Page.TabIndex = 1;
+            this.LogIn_Page.Text = "Log In";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password: ";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(122, 129);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(166, 21);
+            this.username.TabIndex = 4;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(122, 171);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(166, 21);
+            this.password.TabIndex = 5;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(160, 52);
+            this.button1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(87, 218);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(270, 255);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 40);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "LOG IN";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 352);
+            this.ClientSize = new System.Drawing.Size(315, 301);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LogIn_Page);
+            this.Controls.Add(this.Title);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Shadow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label LogIn_Page;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Button button1;
+
 
     }
 }
