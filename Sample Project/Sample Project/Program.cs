19bd17161275a5c9121d9cfe8ceb0abe56dcf4e1
Ric Ian Jamora <yan2_1447@yahoo.com>
2022-09-05 13:26:43 +0800

    class

--- Sample Project/Sample Project/Program.cs
+++ Sample Project/Sample Project/Program.cs
@@ -22,7 +22,7 @@ namespace Sample_Project
             BonusSkins.Register();
             SkinManager.EnableFormSkins();
             UserLookAndFeel.Default.SetSkinStyle("DevExpress Style");
-            Application.Run(new Form1());
+            Application.Run(new LogIn());
         }
     }
 }
