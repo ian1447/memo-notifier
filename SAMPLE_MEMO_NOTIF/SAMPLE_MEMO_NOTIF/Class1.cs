a3e71486f7106748f031314974442d3b950e4103
Kitt Cymonne Japson <japson.kittcymonne@gmail.com>
2022-09-06 08:26:50 +0800

    xfxv

---
+++ SAMPLE_MEMO_NOTIF/SAMPLE_MEMO_NOTIF/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace SAMPLE_MEMO_NOTIF
+{
+    class MySqlConnectivity
+    {
+        public static MySqlConnection conn = null;
+
+        public MySqlConnection Connection() {
+            conn = new MySqlConnection("datasource=localhost;database=memo_notif;port=3306;password=password");
+            return conn;
+        }
+
+        public bool isConnection { 
+            get {
+                try{
+                    if (conn.State == System.Data.ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+                    
+                    return true;
+                } catch {
+                    return false;
+                }
+            }
+        }
+    }
+}
